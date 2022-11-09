79e11e7d54c8f1315acec7d4921786f2c2588d5c
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-09 21:35:33 +0700

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,19 @@ class MyComponent extends React.Component {
         age: 26
     };
 
+    handleClick = (event) => {
+        this.setState({
+            name: 'Eric',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+    }
+
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and i'm from {this.state.address}
+                My name is {this.state.name} and i'm {this.state.age}
+                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
             </div>
         );
     }
