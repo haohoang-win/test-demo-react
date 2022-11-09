14e178f3f04e5c939c83fb624c6e87ae4a779b03
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-09 22:37:48 +0700

    video 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DisplayInfor from "./DisplayInfor";
 import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
@@ -6,9 +7,13 @@ class MyComponent extends React.Component {
 
     // JSX
     render() {
+        const myArr = ['123', 'abc', 23]
         return (
             <div>
                 <UserInfor />
+                <br />
+                <br />
+                <DisplayInfor name="Hao Hoang123" age={26} myInfor={myArr} />
             </div>
         );
     }
