ab914233e208810632dcd86378870b6c49486fac
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-09 21:01:21 +0700

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: "Hao Hoang",
+        address: "hoo.hoang.hao",
+        age: 26
+    };
+
     // JSX
     render() {
         return (
-            <div>My first component
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and i'm from {this.state.address}
             </div>
         );
     }
