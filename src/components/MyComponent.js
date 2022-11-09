6bd3da0d4a3204f315e13c12dab3748c3bb7a266
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-09 22:08:59 +0700

    video 22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,43 +1,14 @@
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
 
-    state = {
-        name: "Hao Hoang",
-        address: "hoo.hoang.hao",
-        age: 26
-    };
-
-    handleClick = (event) => {
-        this.setState({
-            name: 'Eric',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    }
 
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and i'm {this.state.age}
-                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
-                    <input
-                        type='text'
-                        onChange={(event) => { this.handleOnChangeInput(event) }}
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfor />
             </div>
         );
     }
