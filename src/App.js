698ef1958af3fdf17b3795ff3d28a7603d124141
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-12 17:50:29 +0700

    video 47

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.scss';
+import Header from './components/Header/Header';
 
 const App = () => {
 
   return (
     <div className="app-container">
-      Hello world
+      <Header />
     </div>
   );
 }
