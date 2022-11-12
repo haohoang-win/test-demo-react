bd9f2b59fa3a94dc3f9440bcaa819b1ed2bf977a
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-12 21:02:58 +0700

    video 52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,25 @@
+import videoHomepage from '../../assets/video-homepage.mp4'
+
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            Home page container
+            <video autoPlay muted loop>
+                <source
+                    src={videoHomepage}
+                    type='video/mp4'
+                />
+            </video>
+            <div className='homepage-content'>
+                <div className='title-1'>There's a better way to ask</div>
+                <div className='title-2'>
+                    You don't want to make a boring form.
+                    And your audience won't answer one. C
+                    reate a typeform instead—and make everyone happy.
+                </div>
+                <div className='title-3'>
+                    <button>Get's started. It's free</button>
+                </div>
+            </div>
         </div>
     )
 }
