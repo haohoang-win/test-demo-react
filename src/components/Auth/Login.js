49fbbb34feb4f972cd46477815fa47fb0b2b6618
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-15 20:59:56 +0700

    video 70

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,18 +1,34 @@
 import { useState } from 'react';
 import './Login.scss'
+import { useNavigate } from 'react-router-dom';
+import { postLogin } from '../../services/apiService';
+import { toast } from 'react-toastify';
 
 const Login = (props) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate()
 
-    const handleLogin = () => {
+    const handleLogin = async () => {
+        // validate
 
+        // submit apis
+        let data = await postLogin(email, password);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
+            navigate('/')
+        }
+
+        if (data && data.EC !== 0) {
+            toast.error(data.EM)
+        }
     }
 
     return (
         <div className="login-container">
             <div className='header'>
-                Don't have an account yet?
+                <span>Don't have an account yet?</span>
+                <button>Sign up</button>
             </div>
             <div className='title col-4 mx-auto'>
                 HoiDanIT
@@ -47,6 +63,11 @@ const Login = (props) => {
                     >
                         Login to HoiDanIT
                     </button>
+                </div>
+                <div className='text-center'>
+                    <span className='back' onClick={() => { navigate('/') }}>
+                        &#60;&#60; Go to Homepage
+                    </span>
                 </div>
             </div>
         </div>
