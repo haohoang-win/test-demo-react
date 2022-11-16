70a2954ac3bf76cda963a324a9be4783e6eef554
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-16 21:54:22 +0700

    video 82

--- src/Layout.js
+++ src/Layout.js
@@ -10,6 +10,15 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Register from './components/Auth/Register';
 import ListQuiz from './components/User/ListQuiz';
+import DetailQuiz from './components/User/DetailQuiz';
+
+const NotFound = () => {
+    return (
+        <div className='alert alert-danger container mt-3'>
+            404. Not found data with your current URL
+        </div>
+    )
+}
 
 const Layout = () => {
     return (
@@ -20,6 +29,8 @@ const Layout = () => {
                     <Route path='users' element={<ListQuiz />} />
                 </Route>
 
+                <Route path='/quiz/:id' element={<DetailQuiz />} />
+
                 <Route path='/admins' element={<Admin />} >
                     <Route index element={<DashBoard />} />
                     <Route path='manage-users' element={<ManageUser />} />
@@ -27,6 +38,8 @@ const Layout = () => {
 
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
+
+                <Route path='*' element={<NotFound />} />
             </Routes>
             <ToastContainer
                 position="top-right"
--- src/components/User/ListQuiz.js
+++ src/components/User/ListQuiz.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { getQuizByUser } from "../../services/apiService";
-import './ListQuiz.scss'
+import './ListQuiz.scss';
+import { useNavigate } from "react-router-dom";
 
 const ListQuiz = (props) => {
+    const navigate = useNavigate()
     const [arrQuiz, setArrQuiz] = useState([])
     useEffect(() => {
         getQuizData()
@@ -26,7 +28,12 @@ const ListQuiz = (props) => {
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
                                 <p className="card-text">{quiz.description}</p>
-                                <button className="btn btn-primary">Start now</button>
+                                <button
+                                    onClick={() => navigate(`/quiz/${quiz.id}`)}
+                                    className="btn btn-primary"
+                                >
+                                    Start now
+                                </button>
                             </div>
                         </div>
                     )
