2371f749767584101c51cf307476be81301f1387
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-16 22:32:43 +0700

    video 83

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from 'lodash'
 
 const DetailQuiz = (props) => {
     const params = useParams()
@@ -12,7 +13,27 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
-        console.log(res);
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answer = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index == 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answer.push(item.answers)
+                    })
+                    return { questionsId: key, answer, questionDescription, image }
+                })
+                .value()
+            console.log(data);
+        }
     }
 
     return (
