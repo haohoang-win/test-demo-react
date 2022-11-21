b7cbafb931d77d39ffe93db5c6820de9d40abf88
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-11-21 22:26:06 +0700

    video 91

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -63,9 +63,13 @@ const postCreateNewQuiz = (description, name, difficulty, image) => {
     return axios.post('api/v1/quiz', data);
 }
 
+const getAllQuizForAdmin = () => {
+    return axios.get(`/api/v1/quiz/all`)
+}
+
 export {
     postCreateNewUser, getAllUsers, putUpdateUser,
     deleteUser, getUserWithPaginate, postLogin,
     postRegister, getQuizByUser, getDataQuiz,
-    postSubmitQuiz, postCreateNewQuiz
+    postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin
 }
