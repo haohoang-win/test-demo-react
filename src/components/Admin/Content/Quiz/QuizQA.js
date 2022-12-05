7ad8f7066ff36457d8e02901758f227da3533d10
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-12-05 22:01:34 +0700

    video 102

--- src/components/Admin/Content/Quiz/QuizQA.js
+++ src/components/Admin/Content/Quiz/QuizQA.js
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 import _ from 'lodash';
 import Lightbox from "react-awesome-lightbox";
 import { getAllQuizForAdmin } from "../../../../services/apiService";
-import { postCreateNewAnswerForQuestion, postCreateNewQuestionForQuiz, getQuizWithQA } from "../../../../services/apiService";
+import { postCreateNewQuestionForQuiz, getQuizWithQA, postUpsertQA } from "../../../../services/apiService";
 import { toast } from 'react-toastify';
 
 const QuizQA = (props) => {
@@ -229,17 +229,32 @@ const QuizQA = (props) => {
         // submit questions
         for (const question of questions) {
             const q = await postCreateNewQuestionForQuiz(+selectedQuiz.value, question.description, question.imageFile);
-            // submit answers
-            for (const answer of question.answers) {
-                await postCreateNewAnswerForQuestion(
-                    answer.description, answer.isCorrect, q.DT.id
-                )
+        }
+
+        let questionsClone = _.cloneDeep(questions);
+        for (let i = 0; i < questionsClone.length; i++) {
+            if (questionsClone[i].imageFile) {
+                questionsClone[i].imageFile = await toBase64(questionsClone[i].imageFile)
             }
         }
 
-        toast.success('Create questions and answers succed!')
-        setQuestions(initQuestions)
+        let res = await postUpsertQA({
+            quizId: selectedQuiz.value,
+            questions: questionsClone
+        })
+
+        if (res && res.EC === 0) {
+            toast.success(res.EM);
+            fetchQuizWithQA();
+        }
     }
+
+    const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
 
     const handlePreviewImage = (questionId) => {
         let questionsClone = _.cloneDeep(questions)
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -107,11 +107,16 @@ const getQuizWithQA = (quizId) => {
     return axios.get(`api/v1/quiz-with-qa/${quizId}`);
 }
 
+const postUpsertQA = (data) => {
+    return axios.post(`api/v1/quiz-upsert-qa`, { ...data });
+}
+
 export {
     postCreateNewUser, getAllUsers, putUpdateUser,
     deleteUser, getUserWithPaginate, postLogin,
     postRegister, getQuizByUser, getDataQuiz,
     postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin,
     putEditQuiz, deleteQuiz, postCreateNewQuestionForQuiz,
-    postCreateNewAnswerForQuestion, postAssignQuiz, getQuizWithQA
+    postCreateNewAnswerForQuestion, postAssignQuiz, getQuizWithQA,
+    postUpsertQA
 }
