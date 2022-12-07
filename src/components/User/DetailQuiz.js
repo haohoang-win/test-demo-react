0d4ec50f2ad720427bf050558b5374dec70d14bc
Hao Hoang <hoo.hoang.hao@gmail.com>
2022-12-07 20:56:04 +0700

    video 103

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -5,6 +5,7 @@ import _ from 'lodash'
 import './DetailQuiz.scss'
 import Question from "./Question";
 import ModalResult from "./ModalResult";
+import RightContent from "./Content/RightContent";
 
 const DetailQuiz = (props) => {
     const params = useParams();
@@ -155,7 +156,9 @@ const DetailQuiz = (props) => {
                 </div>
             </div>
             <div className="right-content">
-                Count down
+                <RightContent
+                    dataQuiz={dataQuiz}
+                />
             </div>
             <ModalResult
                 show={isShowModalResult}
